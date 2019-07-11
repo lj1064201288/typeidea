69ccaf9831d4f16b9d2e084005d8dc32bc06d1da
lj1064201288 <1064201288@qq.com>
2019-07-11 22:30:25 +0800

    add_blog

--- typeidea/blog/models.py
+++ typeidea/blog/models.py
@@ -21,6 +21,9 @@ class Category(models.Model):
     class Meta:
         verbose_name = verbose_name_plural = '分类'
 
+    def __str__(self):
+        return self.name
+
 
 # 标签
 class Tag(models.Model):
